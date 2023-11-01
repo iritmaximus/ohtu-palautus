44bceba8cff7d65e82b50f58a090e30313a7139c
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2023-11-01 17:23:43 +0200

    refactored nhl app to utilize dependency injection

---
+++ viikko1/nhl-statistics-1/src/index.py
@@ -0,0 +1,23 @@
+from player_reader import PlayerReader
+from statistics_service import StatisticsService
+
+URL = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
+
+def main():
+    player_reader = PlayerReader(URL)
+    stats = StatisticsService(player_reader)
+
+    philadelphia_flyers_players = stats.team("PHI")
+    top_scorers = stats.top(10)
+
+    print("Philadelphia Flyers:")
+    for player in philadelphia_flyers_players:
+        print(player)
+
+    print("Top point getters:")
+    for player in top_scorers:
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
