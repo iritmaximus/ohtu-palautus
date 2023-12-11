bdf5f6153011613e160afb769b78ebd7072d186b
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2023-12-11 22:57:16 +0200

    viikko 6, 3 asti

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,69 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And, HasAtLeast, HasFewerThan, PlaysIn, Not, All, Or
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    matcher = And(
+        HasAtLeast(5, "goals"),
+        HasAtLeast(20, "assists"),
+        PlaysIn("PHI")
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+
+    print()
+    matcher = And(
+        Not(HasAtLeast(2, "goals")),
+        PlaysIn("NYR")
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+
+
+    print()
+    matcher = And(
+        HasFewerThan(2, "goals"),
+        PlaysIn("NYR")
+    )
+    for player in stats.matches(matcher):
+        print(player)
+
+    matcher = Or(
+        HasAtLeast(45, "goals"),
+        HasAtLeast(70, "assists")
+    )
+    print()
+    for player in stats.matches(matcher):
+        print(player)
+
+
+
+
+    matcher = And(
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
+    )
+    print()
+    for player in stats.matches(matcher):
+        print(player)
+
+
+
+
+    filtered_with_all = stats.matches(All())
+    print()
+    print(len(filtered_with_all))
+
+
+if __name__ == "__main__":
+    main()
